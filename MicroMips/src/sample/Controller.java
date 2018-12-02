618a92647c37180085b284c556bac98b2cb6d157
Carl James M. Raymundo <36413714+carlraymundo@users.noreply.github.com>
2018-12-02 09:44:41 +0800

    updated Controller + View

--- MicroMips/src/sample/Controller.java
+++ MicroMips/src/sample/Controller.java
@@ -1,6 +1,38 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+
 public class Controller {
 
+    @FXML
+    private TextArea instructionsArea;
+
+    @FXML
+    private TableView<?> opcodeTable;
+
+    @FXML
+    private TableView<?> registerTable;
+
+    @FXML
+    private TableView<?> dataTable;
+
+    @FXML
+    private Button GotoBtn;
+
+    @FXML
+    private Button LoadBtn;
+
+    @FXML
+    private Button NextBtn;
+
+    @FXML
+    private Button CycleNextBtn;
+
+    @FXML
+    private TableView<?> cyclesTable;
 
 }
+
